e1bb60a96cac2d66bd7b7b00b21be6fff0d7b557
Gregor Donabauer <gregor.donabauer@gmx.de>
2021-12-09 16:23:51 +0100

    extraction of more detailed features

--- data_preprocessing/feature_extraction.py
+++ data_preprocessing/feature_extraction.py
@@ -1,3 +1,5 @@
+import os.path
+import json
 from flair.embeddings import TransformerDocumentEmbeddings, WordEmbeddings, DocumentPoolEmbeddings
 from flair.data import Sentence
 import numpy as np
@@ -7,29 +9,45 @@ def text_embeddings(text_array, model='bert-base-uncased'):
     embedding = TransformerDocumentEmbeddings(model)
     embedding_array = []
     for sentence in text_array:
-        sent = Sentence(sentence)
-        embedding.embed(sent)
-        embedding_array.append(sent.embedding.cpu().detach().numpy())
+        if sentence == '' or sentence == ' ':
+            embedding_array.append(np.zeros(768))
+        else:
+            sent = Sentence(sentence)
+            embedding.embed(sent)
+            embedding_array.append(sent.embedding.cpu().detach().numpy())
     return np.asarray(embedding_array)
 
 
 def get_news_features(news_data):
-    return news_data['text']
+    return news_data['title'] + '. ' + news_data['text']
+
+
+def get_summaries(news_id, dataset='politifact', subset='fake', *args):
+    file_path = '../data/fakenewsnet_dataset/' + dataset + '/' + subset + '/' + news_id + '/summary.json'
+    output = ''
+    if os.path.isfile(file_path):
+        with open(file_path) as json_file:
+            data = json.load(json_file)
+        for summary_type in args:
+            output += data[summary_type] + ' '
+        return output.strip()
+    else:
+        return output
 
 
 def get_tweet_features(tweet_data):
-    # return [tweet_data['text'], tweet_data['retweet_count'], tweet_data['favorite_count']]
-    return [tweet_data['retweet_count'], tweet_data['favorite_count']]
+     return [tweet_data['text'], tweet_data['retweet_count'], tweet_data['favorite_count']]
 
 
 def get_user_features(user_data):
-    #return [user_data['description'], user_data['followers_count'],  user_data['friends_count'], user_data['favourites_count'],
-    #        user_data['statuses_count']]
-    return [user_data['followers_count'], user_data['friends_count'], user_data['favourites_count'], user_data['statuses_count']]
+    return [user_data['description'], user_data['followers_count'],  user_data['friends_count'], user_data['favourites_count'],
+            user_data['statuses_count']]
+
 
 
 if __name__ == '__main__':
-    test_array = ['hello', 'test', 'an string']
+
+    test_array = ['hello', 'test', 'a string', '']
     embedded_array = text_embeddings(test_array)
-    print(len(embedded_array))
-    print(embedded_array[0].shape)
+    print(embedded_array.shape)
+    print(embedded_array[-1].shape)
